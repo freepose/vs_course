1c523142c23d7460624df82ce57a72461f33ffc0
giantcui <920775780@qq.com>
2018-03-23 17:25:25 +0800

    format

--- data_structure/src/include/LinkNode.h
+++ data_structure/src/include/LinkNode.h
@@ -4,55 +4,56 @@
 #include<iostream>
 using namespace std;
 
-typedef struct LNode{
+typedef struct LNode {
 	int data;
 	struct LNode *next;
 }LinkNode;
 
 
-// 
-void CreateListF(LinkNode *&L,int a[],int n)
+// this is a test
+// this is a test again.
+void CreateListF(LinkNode *&L, int a[], int n)
 {
 	LinkNode *s;
 	L = new LinkNode;
-	L->next=0;
-	for(int i=0;i<n;i++){
-		s=new LinkNode;
-		s->data=a[i];
-		s->next=L->next;
-		L->next=s;
+	L->next = 0;
+	for (int i = 0; i<n; i++) {
+		s = new LinkNode;
+		s->data = a[i];
+		s->next = L->next;
+		L->next = s;
 	}
 }
 
-void CreateListR(LinkNode *&L,int a[],int n)
+void CreateListR(LinkNode *&L, int a[], int n)
 {
 	LinkNode *s, *r;
 	L = new LinkNode;
-	r=L;
-	for(int i=0;i<n;i++){
-		s=new LinkNode;
-		s->data=a[i];
-		r->next=s;
-		r=s;
+	r = L;
+	for (int i = 0; i<n; i++) {
+		s = new LinkNode;
+		s->data = a[i];
+		r->next = s;
+		r = s;
 	}
-	r->next=0;
+	r->next = 0;
 }
 
 
 void DispList(LinkNode *L)
 {
-	LinkNode *p=L->next;
-	while(p!=0){
-		cout<<"\t"<<p->data;
-		p=p->next;
+	LinkNode *p = L->next;
+	while (p != 0) {
+		cout << "\t" << p->data;
+		p = p->next;
 	}
-	cout<<endl;
+	cout << endl;
 }
 
 void DestroyList(LinkNode *L)
 {
 	LinkNode *pre = L, *p = L->next;
-	while(p != 0)
+	while (p != 0)
 	{
 		delete pre;
 		pre = p;
