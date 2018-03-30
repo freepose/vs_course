1b02e302449e97a70115b53c7f67c01b732ac0f2
FpsNoob <18850128056@163.com>
2018-03-30 22:52:46 +0800

    Update part of the SqQueue Code

---
+++ data_structure/SqQueue.h
@@ -0,0 +1,45 @@
+#ifndef SQQUEUE_H
+#define SQQUEUE_H 1
+
+#include "basic.h"
+
+template <typename T> struct SqQueue {
+	T data[MAX_SIZE];
+	int front;
+	int rear;
+};
+
+
+template <typename T> void InitQueue(SqQueue<T> *&q)
+{
+	q = new SqQueue<T>;
+	q->front = q->rear = -1;
+}
+
+
+template <typename T> void DestroyQueue(SqQueue<T> *&q)
+{
+	delete q;
+}
+
+
+template <typename T> bool QueueEmpty(SqQueue<T> *q)
+{
+	return(q->front == q->rear);
+}
+
+
+template <typename T> bool enQueue(SqQueue<T> *&q, T e)
+{
+	if (q->front == MAX_SIZE - 1) {  //¶ÓÂúÉÏÒç³ö
+		return false;
+	}
+	q->rear++;
+	e = q->data[q->rear];
+	return true;
+}
+
+
+
+
+#endif
