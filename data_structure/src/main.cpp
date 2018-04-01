29255f0cca4bb80f74ed2bc186ada857bdb2b028
giantcui <920775780@qq.com>
2018-04-01 16:13:31 +0800

    about DoubleLinkQueue

--- data_structure/src/main.cpp
+++ data_structure/src/main.cpp
@@ -4,6 +4,7 @@
 #include"include/SqStack.h"
 #include"include/LinkStackNode.h"
 #include"include/SqQueue.h"
+#include"include/DLinkQueue.h"
 
 int main()
 {
@@ -14,8 +15,10 @@ int main()
 	//SqStackExample();
 	//LinkStackNodeExample();
 
-	SqQueueExample();
+	//SqQueueExample();
+	DLinkQueueExamples();
 	//mgpath_example();
+
 	
 	return 0;
 }
